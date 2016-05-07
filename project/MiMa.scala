f4c385cdd3645fb1f5cf9ae8f6048fdbb9e235a8
leonidb <leonid.bakaleynik@gmail.com>
2016-05-07 10:04:57 +0300

    Added Mima plugin config to ignore binary compatibility issues

--- project/MiMa.scala
+++ project/MiMa.scala
@@ -715,6 +715,14 @@ object MiMa extends AutoPlugin {
 
         // #19390 Add flow monitor
         ProblemFilters.exclude[ReversedMissingMethodProblem]("akka.stream.scaladsl.FlowOpsMat.monitor")
+      ),
+      "2.4.4" -> Seq(
+        // #20257 Snapshots with PersistentFSM (experimental feature)
+        ProblemFilters.exclude[ReversedMissingMethodProblem]("akka.persistence.serialization.MessageFormats#PersistentStateChangeEventOrBuilder.getTimeoutNanos"),
+        ProblemFilters.exclude[ReversedMissingMethodProblem]("akka.persistence.serialization.MessageFormats#PersistentStateChangeEventOrBuilder.hasTimeoutNanos"),
+        ProblemFilters.exclude[ReversedMissingMethodProblem]("akka.persistence.fsm.PersistentFSM.saveStateSnapshot"),
+        ProblemFilters.exclude[ReversedMissingMethodProblem]("akka.persistence.fsm.PersistentFSM.akka$persistence$fsm$PersistentFSM$$currentStateTimeout"),
+        ProblemFilters.exclude[ReversedMissingMethodProblem]("akka.persistence.fsm.PersistentFSM.akka$persistence$fsm$PersistentFSM$$currentStateTimeout_=")
       )
     )
   }
